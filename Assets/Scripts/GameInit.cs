2766adb79d6fb461691322f9327ef0c9e8cdedbd
MizuRyujin <miguelrf95@gmail.com>
2022-11-26 17:51:36 +0000

    Create simple player movement

--- Assets/Scripts/GameInit.cs
+++ Assets/Scripts/GameInit.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameInit : MonoBehaviour
 {
+    private List<AsyncOperation> _ops;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -10,5 +14,29 @@ public class GameInit : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        LoadFirstScenes();
+    }
+
+    private void LoadFirstScenes()
+    {
+        _ops = new List<AsyncOperation>(){
+            SceneManager.LoadSceneAsync("CityLevel", LoadSceneMode.Additive),
+            SceneManager.LoadSceneAsync("GameplayScene", LoadSceneMode.Additive),
+        };
+        StartCoroutine(UnloadInitScene());
+    }
+
+    private IEnumerator UnloadInitScene()
+    {
+        for (int i = 0; i < _ops.Count; i++)
+        {
+            if (!_ops[i].isDone)
+            {
+                yield return null;
+            }
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("CityLevel"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByBuildIndex(0));
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,98 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _maxSpeed;
-    [SerializeField] private float _acceleration;
+    [SerializeField] private float _acceleration = 10f;
+    [SerializeField] private float _jumpForce = 100f;
+    [SerializeField] private float _jumpTime = 2f;
+    [SerializeField] private float _rotationFactor = 5f;
 
-    private CharacterController _controller;
-	
+    private bool _isGrounded;
+    private Rigidbody _rb;
+    private Vector3 _playerInput;
+    private Vector2 _mouseInput;
+    private float _jumpTimer;
+
+
     /// <summary>
-    /// Start is called before the first frame update
-	/// </summary>>
-    private void Start()
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _jumpTimer = _jumpTime;
     }
-	
-	/// <summary>>
+
+    /// <summary>>
     /// Update is called once per frame
-	/// </summary>>
+    /// </summary>>
     private void Update()
     {
-
+        _playerInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        RotateCamera();
     }
 
-    private void Movement()
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void FixedUpdate()
     {
-        float xAxis, yAxis;
+        IsGrounded();
+        PlayerMove();
+        PlayerJump();
+    }
 
-        if (Input.GetAxisRaw("Horizontal") != 0)
+
+    private void IsGrounded()
+    {
+        Collider[] colliders = new Collider[1];
+        int hits = Physics.OverlapSphereNonAlloc(transform.position, 0.1f, colliders, LayerMask.GetMask("Ground"));
+        if (hits > 0)
         {
-            
+            _isGrounded = true;
         }
+        else
+        {
+            _isGrounded = false;
+        }
+    }
+
+    private void PlayerMove()
+    {
+        Vector3 moveDir = transform.TransformDirection(_playerInput) * _acceleration;
+
+        var currSpeed = _rb.velocity;
+        _rb.velocity = new Vector3(moveDir.x, _rb.velocity.y, moveDir.z);
+        // if (_rb.velocity.sqrMagnitude > 10)
+        // {
+        //     _rb.velocity = currSpeed;
+        // }
+    }
+
+    private void RotateCamera()
+    {
+        transform.Rotate(transform.up, _mouseInput.x * _rotationFactor);
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(_isGrounded || _jumpTimer > 0f)
+            {
+                _rb.AddForce(transform.up * _jumpForce);
+                _jumpTimer -= 0.1f * Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.1f);
     }
 }
